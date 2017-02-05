1d7b263f06e5e138afc8d22365f459a82fa997ef
Vincent <AM55720@ens.etsmtl.ca>
2017-02-05 15:53:29 -0500

    Create "Pro version" page

--- mobile/src/pages/pro-version/pro-version.ts
+++ mobile/src/pages/pro-version/pro-version.ts
@@ -13,6 +13,44 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ProVersionPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  //private minimumExpirationDate: String;
+  //private maximumExpirationDate: String;
+  private months = [];
+  private years = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    /*this.minimumExpirationDate = new Date().toISOString();
+    let maxDate = new Date();
+    maxDate.setFullYear(maxDate.getFullYear() + 20);
+    this.maximumExpirationDate = maxDate.toISOString();*/
+    for (let i = 1; i <= 12; i++) {
+      let month = {
+        "value": i,
+        "text": i < 10 ? "0" + i.toString() : i.toString()
+      };
+      this.months.push(month);
+      //this.months[i].value = i;
+      //this.months[i].text = i < 10 ? "0" + i.toString() : i.toString();
+    }
+
+    let presentYear = new Date().getFullYear();
+    for (let i = presentYear; i <= presentYear + 20; i++) {
+      let year = {
+        "value": i,
+        "text": i
+      };
+      this.years.push(year);
+    }
+  }
+
+  // for testing
+  onChange(selectedItem) {
+    console.log("Selected item: " + selectedItem);
+  }
+
+  // carry out payment to buy the version without ad
+  confirmPayment() {
+    console.log("Confirm payment");
+  }
 
 }
