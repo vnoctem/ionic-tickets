c01eb8f273a7100e054d9f56bcfd2439ddaf7804
Chun <chun-hei.chu.1@etsmtl.net>
2017-03-05 20:22:49 -0500

    Manage error in authentication page

--- mobile/src/pages/authentication/authentication.ts
+++ mobile/src/pages/authentication/authentication.ts
@@ -2,6 +2,7 @@ import { TicketsPage } from './../tickets/tickets';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AuthController } from './../../providers/auth-controller';
+import { HttpHelper } from './../../providers/http-helper';
 
 /*
   Generated class for the Authentication page.
@@ -19,7 +20,7 @@ export class AuthenticationPage {
   private password: string;
   private error: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public authCtrl: AuthController) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public authCtrl: AuthController, public helper: HttpHelper) { }
 
   login() {
     this.authCtrl.postLogin({
@@ -30,7 +31,11 @@ export class AuthenticationPage {
         this.navCtrl.setRoot(TicketsPage);
       })
       .catch(err => {
-        this.error = err._body.message;
+        if (err.status == 0) { // api unavailable
+          this.helper.showToast('API indisponible');
+        } else if (err._body.message) {
+          this.error = err._body.message;
+        }
       });
   }
 
