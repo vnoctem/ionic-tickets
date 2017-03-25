04296c90748bb1f748a2c2a4434afa7d8ccf57b0
Vincent <AM55720@ens.etsmtl.ca>
2017-03-25 11:30:38 -0400

    Change GUIDs to test Ticket validation API

--- models/tickets.js
+++ models/tickets.js
@@ -2,50 +2,6 @@ module.exports = [{
     'userId': 1,
     'tickets': [
         {
-            'id': 145625,
-            'showName': 'Borgeous',
-            'artistName': 'Borgeous',
-            'prix': 24.00,
-            'datetime': '2017-02-18 22:00',
-            'location': 'New City Gas',
-            'address': '950 Rue Ottawa, Montréal, QC H3C 1S4',
-            'poster': '/images/shows/borgeous.jpg',
-            'GUID': '0bd420bb-c83f-4699-b843-306dcd6d516a'
-        },
-        {
-            'id': 145625,
-            'showName': 'Borgeous',
-            'artistName': 'Borgeous',
-            'prix': 24.00,
-            'datetime': '2017-02-18 22:00',
-            'location': 'New City Gas',
-            'address': '950 Rue Ottawa, Montréal, QC H3C 1S4',
-            'poster': '/images/shows/borgeous.jpg',
-            'GUID': '0bd420bb-c83f-4699-b843-306dcd6d517a'
-        },
-        {
-            'id': 145625,
-            'showName': 'Borgeous',
-            'artistName': 'Borgeous',
-            'prix': 24.00,
-            'datetime': '2017-02-18 22:00',
-            'location': 'New City Gas',
-            'address': '950 Rue Ottawa, Montréal, QC H3C 1S4',
-            'poster': '/images/shows/borgeous.jpg',
-            'GUID': '0bd420bb-c83f-4699-b843-306dcd6d518a'
-        },
-        {
-            'id': 145625,
-            'showName': 'Borgeous',
-            'artistName': 'Borgeous',
-            'prix': 24.00,
-            'datetime': '2017-02-18 22:00',
-            'location': 'New City Gas',
-            'address': '950 Rue Ottawa, Montréal, QC H3C 1S4',
-            'poster': '/images/shows/borgeous.jpg',
-            'GUID': '0bd420bb-c83f-4699-b843-306dcd6d519a'
-        },
-        {
             'id': 421568,
             'showName': 'Luzia',
             'artistName': 'Cirque du Soleil',
@@ -54,7 +10,7 @@ module.exports = [{
             'location': 'Grand Chapiteau',
             'address': 'Quai Jacques-Cartier, rue de la Commune Est, Montréal, QC H2Y 1J1',
             'poster': '/images/shows/luzia.jpg',
-            'GUID': '25121906-74f7-4ec5-9016-9d06e10d72cc'
+            'GUID': '11aa'
         },
         {
             'id': 423156,
@@ -65,7 +21,7 @@ module.exports = [{
             'location': 'Centre Bell',
             'address': '1909 Avenue des Canadiens-de-Montréal, Montréal, QC H4B 5G0',
             'poster': '/images/shows/realHousewives.jpg',
-            'GUID': '0bd420bb-c83f-4699-b843-306dcd6d526a'
+            'GUID': '22bb'
         },
         {
             'id': 145625,
@@ -76,7 +32,7 @@ module.exports = [{
             'location': 'New City Gas',
             'address': '950 Rue Ottawa, Montréal, QC H3C 1S4',
             'poster': '/images/shows/borgeous.jpg',
-            'GUID': '0bd420bb-c83f-4699-b843-306dcd6d536a'
+            'GUID': '33cc'
         },
         {
             'id': 111232,
@@ -86,7 +42,7 @@ module.exports = [{
             'location': 'Centre de la petitesse',
             'address': ' Pierrefonds, QC H4B 5G0',
             'poster': '/images/shows/petitshow.png',
-            'GUID': '0bd420bb-c83f-4699-b843-306dcd6d546a'
+            'GUID': '44dd'
         },
         {
             'id': 417231,
@@ -96,7 +52,49 @@ module.exports = [{
             'location': 'Centre Bell',
             'address': '1909 Avenue des Canadiens-de-Montréal, Montréal, QC H4B 5G0',
             'poster': '/images/shows/mystere.jpg',
-            'GUID': '0bd420bb-c83f-4699-b843-306dcd6d556a'
+            'GUID': '55ee'
+        },
+        {
+            'id': 417567,
+            'showName': 'Opera',
+            'artistName': 'who dat',
+            'datetime': '2017-04-23 16:13',
+            'location': 'Centre Bell',
+            'address': '1909 Avenue des Canadiens-de-Montréal, Montréal, QC H4B 5G0',
+            'poster': '/images/shows/opera.jpg',
+            'GUID': '66ff'
+        },
+        {
+            'id': 123567,
+            'showName': 'Volta',
+            'artistName': 'who dat',
+            'datetime': '2017-04-23 16:13',
+            'location': 'Centre Bell',
+            'address': '1909 Avenue des Canadiens-de-Montréal, Montréal, QC H4B 5G0',
+            'poster': '/images/shows/opera.jpg',
+            'GUID': '77gg'
+        },
+        {
+            'id': 982321,
+            'showName': 'Borgeous',
+            'artistName': 'Borgeous',
+            'prix': 24.00,
+            'datetime': '2017-02-18 22:00',
+            'location': 'New City Gas',
+            'address': '950 Rue Ottawa, Montréal, QC H3C 1S4',
+            'poster': '/images/shows/borgeous.jpg',
+            'GUID': '0bd420bb-c83f-4699-b843-306dcd6d516a'
+        },
+        {
+            'id': 543283,
+            'showName': 'Luzia',
+            'artistName': 'Cirque du Soleil',
+            'prix': 65.00,
+            'datetime': '2017-06-26 21:00',
+            'location': 'Grand Chapiteau',
+            'address': 'Quai Jacques-Cartier, rue de la Commune Est, Montréal, QC H2Y 1J1',
+            'poster': '/images/shows/luzia.jpg',
+            'GUID': '25121906-74f7-4ec5-9016-9d06e10d72cc'
         }
     ]
 }];
